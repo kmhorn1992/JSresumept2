a58633c6d63b4a594de02baa0e518f7734cb440a
Adrian <kmhorn1992@icloud.com>
2017-09-28 16:35:48 -0500

    almost there

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
-
-
 /*Arrays*/
-var friends = ['Monte', 'Nic', 'Miata','Bradley','Kendia'];
+var friends = ['Monte', 'Nic', 'Miata', 'Bradley', 'Kendia'];
 
-/*Variables*/
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('DOMContentLoaded');
 
-/*Functions*/
-for (k = 0; k < friends.length; k++){
-    console.log(friends[k] + ':');
-    for (var l = 99; l > 0; l--) { 
-        console.log(l + ' lines of code in the file, ' + l + ' lines of code; ' + friends[k] + ' strikes one out, clears it all out ' + (l-1) + ' lines of code in the file');
-        if (l < 2) {
-            console.log(l + ' line of code in the file,' + ' line of code;'  + friends[k] + ' strikes it out, cleared it all out, no more lines of code in the file');
-       
 
+
+
+    /*Variables*/
+    var btn = document.getElementById('mybtn');
+
+
+
+
+    /*Functions*/
+
+    mybtn.addEventListener('click', function () {
+        for (k = 0; k < friends.length; k++) {
+            var fransDiv = document.createElement('div');
+            fransDiv.className = 'friend';
+            document.body.appendChild(fransDiv);
+            var fransHeading = document.createElement('h3');
+            var fransName = document.createTextNode(friends[k]);
+            fransHeading.appendChild(fransName);
+
+            for (var l = 99; l > 0; l--) {
+                var para = document.createElement('p');
+                if (l === 2) {
+                    var lyrics = document.createTextNode(l + ' lines of code in the file, ' + l + ' lines of code; ' + friends[k] + ' strikes one out, clears it all out ' + (l - 1) + ' lines of code in the file');
+                } else if (l === 1) {
+                    var lyrics = document.createTextNode(l + ' line of code in the file,' + ' line of code;' + friends[k] + ' strikes it out, cleared it all out, no more lines of code in the file');
+                } else {
+                    var lyrics = document.createTextNode(l + ' lines of code in the file, ' + l + ' lines of code; ' + friends[k] + ' strikes one out, clears it all out ' + (l - 1) + ' lines of code in the file');
+
+                    para.appendChild(lyrics);
+                    fransDiv.appendChild(para);
+
+
+
+                }
+            }
         }
-    }
 
-}
-
+    });
+});
